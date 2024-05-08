535f26fc692e45f90a605878e68ca6241d9db4d0
anshul-79 <anshulsati79@gmail.com>
2024-05-08 12:47:19 +0530

    minor dashoard changes

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -26,7 +26,7 @@
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
+                <a href="home" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
               </div>
             </div>
           </div>
