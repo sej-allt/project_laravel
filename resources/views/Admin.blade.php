aea25315db957411204aa2f2fb823ac2f5fe0c7b
sej-allt <sejalltt@gmail.com>
2024-05-13 12:58:36 +0530

    something

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -1,5 +1,4 @@
 @extends('layout.header')
-
 @section('header_content')
     <a href="Admin_home" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
 @endsection
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -16,7 +16,7 @@
   ```
 -->
 <div class="min-h-full">
-    <nav class="bg-gray-800 h-12">
+    <nav class="bg-gray-800 ">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
