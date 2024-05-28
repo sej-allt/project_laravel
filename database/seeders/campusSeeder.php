f3f98f94bafdd37e24e0fda88cac7d34bfb8eac0
Sneha Jain <jainsneha2948@gmail.com>
2024-05-28 22:04:02 +0530

    Temp Data upload

---
+++ database/seeders/campusSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class campusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $content = [
+            'campus_id'=>'GEHUD',
+            'campus_name'=>'Graphic Era Hill University Dehradun',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ];
+        DB::table('campus')->insert($content);
+        $content = [
+            'campus_id'=>'GEHUB',
+            'campus_name'=>'Graphic Era Hill University Bhimtal',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ];
+        DB::table('campus')->insert($content);
+        $content = [
+            'campus_id'=>'GEHUH',
+            'campus_name'=>'Graphic Era Hill University Haldwani',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ];
+        DB::table('campus')->insert($content);
+        $content = [
+            'campus_id'=>'GEU',
+            'campus_name'=>'Graphic Era University',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ];
+        DB::table('campus')->insert($content);
+        $content = [
+            'campus_id'=>'UPES',
+            'campus_name'=>'University of Petrolreum and Energy Studies',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ];
+        DB::table('campus')->insert($content);
+    }
+}
